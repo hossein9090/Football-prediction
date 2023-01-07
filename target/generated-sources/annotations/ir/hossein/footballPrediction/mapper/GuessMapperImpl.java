5cf3f7f242d58713099edb72a65800c41cb97960
Hossein9090 <hossein.525h.g@gmail.com>
2023-01-07 07:35:32 +0330

    update maven dependencies

--- target/generated-sources/annotations/ir/hossein/footballPrediction/mapper/GuessMapperImpl.java
+++ target/generated-sources/annotations/ir/hossein/footballPrediction/mapper/GuessMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-09-11T11:46:43+0430",
+    date = "2022-12-31T12:16:37+0330",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0-262 (OpenLogic-OpenJDK)"
 )
 @Component
